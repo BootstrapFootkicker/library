b1e97f46351ab60cf1efcd80edb6d6712322881c
BootstrapFootkicker <89753423+BootstrapFootkicker@users.noreply.github.com>
2022-12-11 16:37:16 -0700

    Book Object Created Book object constructor Created Book prototype info function

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+function Book(title, author, pages, read){
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.read=read;
+}
+
+Book.prototype.info= function (){
+    return(`${this.title} by ${this.author}, ${this.pages}. pages read = ${this.read}`);
+}
+
+trashBook= new Book('its a me','mario',55,true);
+
+
+console.log(trashBook.info());
