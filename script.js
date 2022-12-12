794e4230058b13a9ef0fdec95b534e9ff15a33b2
BootstrapFootkicker <89753423+BootstrapFootkicker@users.noreply.github.com>
2022-12-11 21:15:39 -0700

    Add to library Created function that takes user input and adds book object to library

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
+let myLibrary = [];
 
-function Book(title, author, pages, read){
-    this.title=title;
-    this.author=author;
-    this.pages=pages;
-    this.read=read;
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-Book.prototype.info= function (){
-    return(`${this.title} by ${this.author}, ${this.pages}. pages read = ${this.read}`);
+Book.prototype.info = function () {
+    return (`${this.title} by ${this.author}, ${this.pages}. pages read = ${this.read}`);
 }
 
-trashBook= new Book('its a me','mario',55,true);
+function addBookToLibrary() {
+    let title = prompt("What is the title of your book?");
+    let author = prompt("Who is the author of this book?");
+    let pages = prompt('How many pages does this book have?');
+    let read = prompt("Did you already read this book?").toLowerCase();
+
+    read = read === "yes";
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+    console.log(myLibrary);
+}
 
 
-console.log(trashBook.info());
+addBookToLibrary()
