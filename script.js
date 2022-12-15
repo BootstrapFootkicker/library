afb0803f7a58ec71666d4108926f55cfc5db5ed3
BootstrapFootkicker <89753423+BootstrapFootkicker@users.noreply.github.com>
2022-12-14 17:23:27 -0700

    Add book div Removed display function Deleted old code Fixed duplicate card issue Replaced extra functions with createbookcard

--- script.js
+++ script.js
@@ -28,7 +28,8 @@ function addBookToLibrary(title, author, pages, read) {
     let book = new Book(title, author, pages, read);
     myLibrary.push(book);
 
-    displayBooks();
+    let bookDiv = createBookCard(title, author, pages, read);
+    mainContainer.appendChild(bookDiv);
 }
 
 function clearDiv(div) {
@@ -36,97 +37,39 @@ function clearDiv(div) {
 }
 
 
-// function createForm() {
-//
-//     let formDiv = document.createElement('form');
-//     formDiv.setAttribute('id','formDiv')
-//
-//     let divContents=document.createElement('div');
-//
-//
-//     let titleDiv = document.createElement("input");
-//     titleDiv.setAttribute("type", "text")
-//     titleDiv.setAttribute("placeholder", "Title");
-//
-//
-//     let authorDiv = document.createElement("input");
-//     authorDiv.setAttribute("type", "text")
-//     authorDiv.setAttribute("placeholder", "Author");
-//
-//     let pagesDiv = document.createElement("input");
-//     pagesDiv.setAttribute("type", "number")
-//     pagesDiv.setAttribute("placeholder", "Pages");
-//
-//     let readLabel = document.createElement("label");
-//     readLabel.setAttribute('for', 'read');
-//     readLabel.appendChild(document.createTextNode("Have you read it?"))
-//
-//     let readDiv = document.createElement("input");
-//     readDiv.setAttribute("type", "checkbox");
-//     readDiv.setAttribute("id", "read");
-//
-//
-//     let submitButton = document.createElement("input");
-//     submitButton.setAttribute("type", "submit");
-//     submitButton.setAttribute("id", 'submit')
-//     submitButton.addEventListener('click', () => submit('submit book', titleDiv.value,
-//         authorDiv.value, pagesDiv.value, document.querySelector('#read').checked));
-//
-//     divContents.classList.add('pop-up-form');
-//     divContents.appendChild(titleDiv);
-//     divContents.appendChild(authorDiv);
-//     divContents.appendChild(pagesDiv);
-//     readLabel.appendChild(readDiv)
-//     divContents.appendChild(readLabel)
-//     divContents.appendChild(submitButton);
-//     formDiv.appendChild(divContents);
-//
-//
-//     return formDiv;
-//
-// }
-
-function displayBooks() {
-
-    for (let bookNumber in myLibrary) {
-
-        let cardDiv = document.createElement("div");
-        cardDiv.setAttribute('id', 'cardDiv' + bookNumber.toString());
+function createBookCard(title, author, pages, read) {
+    let cardDiv = document.createElement("div");
+    cardDiv.setAttribute('id', title + 'Card');
 
 
-        let tileDiv = document.createElement("div");
-        let authorDiv = document.createElement("div");
-        let pagesDiv = document.createElement("div");
-        let readDiv = document.createElement("button");
-        let removeButton = document.createElement("button")
+    let tileDiv = document.createElement("div");
+    let authorDiv = document.createElement("div");
+    let pagesDiv = document.createElement("div");
+    let readDiv = document.createElement("button");
+    let removeButton = document.createElement("button")
 
-        tileDiv.setAttribute('id', 'titleDiv' + bookNumber.toString());
+    tileDiv.setAttribute('id', title + 'titleDiv');
 
-        removeButton.textContent = "Remove";
-        removeButton.setAttribute('id', 'removeButton' + bookNumber.toString());
-        removeButton.addEventListener("click", removeBook);
+    removeButton.textContent = "Remove";
+    removeButton.setAttribute('id', title + 'removeButton');
+    removeButton.addEventListener("click", removeBook);
 
-        readDiv.setAttribute('id', 'toggleRead' + bookNumber.toString());
-        readDiv.addEventListener("click", ChangeReadStatus);
+    readDiv.setAttribute('id', title + 'toggleRead');
+    readDiv.addEventListener("click", ChangeReadStatus);
 
-        tileDiv.textContent = myLibrary[bookNumber].title;
-        authorDiv.textContent = myLibrary[bookNumber].author;
-        pagesDiv.textContent = myLibrary[bookNumber].pages;
-        readDiv.textContent = myLibrary[bookNumber].read;
+    tileDiv.textContent = title
+    authorDiv.textContent = author;
+    pagesDiv.textContent = pages;
+    readDiv.textContent = read;
 
-        cardDiv.classList.add('book-card');
+    cardDiv.classList.add('book-card');
 
-        cardDiv.appendChild(tileDiv);
-        cardDiv.appendChild(authorDiv);
-        cardDiv.appendChild(pagesDiv);
-        cardDiv.appendChild(readDiv);
-        cardDiv.appendChild(removeButton);
-
-        mainContainer.appendChild(cardDiv);
-
-    }
-
-
+    cardDiv.appendChild(tileDiv);
+    cardDiv.appendChild(authorDiv);
+    cardDiv.appendChild(pagesDiv);
+    cardDiv.appendChild(readDiv);
+    cardDiv.appendChild(removeButton);
+    return cardDiv;
 }
 
 function submit(title, author, pages, read) {
@@ -154,7 +97,7 @@ function ChangeReadStatus() {
     } else {
         myLibrary[bookIndex].read = "Read";
     }
-    displayBooks();
+    //  displayBooks();
 }
 
 function removeBook() {
@@ -164,15 +107,12 @@ function removeBook() {
     let isTitle = (element) => element.title === bookName;
     let bookIndex = myLibrary.findIndex(isTitle);
     myLibrary.splice(bookIndex, 1);
-    displayBooks();
+//    displayBooks();
 
 }
 
 
 addBookButton.addEventListener('click', () => {
-    // let popUpDiv = createForm();
-    // mainContainer.appendChild(popUpDiv);
-    // addBookButton.disabled=true;
     if (form == null) return;
     overlay.classList.add('active');
     form.classList.add('active');
@@ -186,12 +126,6 @@ overlay.addEventListener('click', () => {
     form.classList.remove('active');
 })
 
-
-// formSubmitButton.addEventListener('click', () => {
-//     submit('submit book', titleDiv.value,
-//         authorDiv.value, pagesDiv.value, document.querySelector('#read').checked)
-//
-// });
 
 formSubmitButton.addEventListener('click', function (e) {
     submit(titleDiv.value, authorDiv.value, pagesDiv.value, document.querySelector('#read').checked);
