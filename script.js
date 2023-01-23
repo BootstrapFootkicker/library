9782a39c76e788ccd0b82e32689b6673e8e83ad6
BootstrapFootkicker <89753423+BootstrapFootkicker@users.noreply.github.com>
2023-01-22 22:07:47 -0700

    classes Changed program to support classes Created Book class Created Library class Remove and Change book status now accept id parameter

--- script.js
+++ script.js
@@ -9,132 +9,146 @@ const pagesDiv = document.querySelector('#formPages');
 const readDiv = document.querySelector('#formRead');
 
 
-let myLibrary = [];
+class Library {
+    constructor() {
+        this.myLibrary = [];
+    }
 
+    get library(){
+        return this.myLibrary;
+    }
+    bookExists(title) {
+        let doesExist;
+        for (let bookNumber in this.myLibrary) {
+            doesExist = (this.myLibrary[bookNumber].title === title);
+            if (doesExist === true) {
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
-
-Book.prototype.info = function () {
-    return (`${this.title} by ${this.author}, ${this.pages}. pages read = ${this.read}`);
-}
-
-function bookExists(title) {
-    let doesExist;
-    for (let bookNumber in myLibrary) {
-        doesExist = (myLibrary[bookNumber].title === title);
-        if (doesExist === true) {
-
-            return doesExist;
+                return doesExist;
+            }
         }
+
+    }
+
+
+    createBookCard(title, author, pages, read) {
+        let cardDiv = document.createElement("div");
+        cardDiv.setAttribute('id', title + 'Card');
+
+
+        let tileDiv = document.createElement("div");
+        let authorDiv = document.createElement("div");
+        let pagesDiv = document.createElement("div");
+
+        let buttonGroup = document.createElement("div");
+        buttonGroup.setAttribute('id', title + 'ButtonGroup')
+        buttonGroup.classList.add('button-group')
+
+        let readDiv = document.createElement("button");
+        let removeButton = document.createElement("button")
+
+        tileDiv.setAttribute('id', title + 'titleDiv');
+
+        removeButton.textContent = "Remove";
+        removeButton.setAttribute('id', title + 'removeButton');
+        removeButton.addEventListener("click", ()=> this.removeBook((title + 'removeButton')));
+
+        readDiv.setAttribute('id', title + 'toggleRead');
+        readDiv.addEventListener("click",()=> this.ChangeReadStatus((title+'ButtonGroup')));
+
+        if (read === 'Read') {
+            readDiv.classList.add('read')
+        } else {
+            readDiv.classList.add('notRead')
+        }
+
+        tileDiv.textContent = title
+        authorDiv.textContent = author;
+        pagesDiv.textContent = pages;
+        readDiv.textContent = read;
+
+        cardDiv.classList.add('book-card');
+
+        cardDiv.appendChild(tileDiv);
+        cardDiv.appendChild(authorDiv);
+        cardDiv.appendChild(pagesDiv);
+
+        buttonGroup.appendChild(readDiv);
+        buttonGroup.appendChild(removeButton);
+
+        cardDiv.appendChild(buttonGroup);
+
+        return cardDiv;
+    }
+
+    addBookToLibrary(title, author, pages, read) {
+
+        let book = new Book(title, author, pages, read);
+        this.myLibrary.push(book);
+        let bookDiv = this.createBookCard(title, author, pages, read);
+        mainContainer.appendChild(bookDiv);
+    }
+
+
+    ChangeReadStatus(id) {
+        let newId = id.toString().substring(0, id.toString().length - 11);
+        console.log(newId);
+        let selector = '#' + newId;
+        console.log(selector);
+        let readButton = document.querySelector(selector + 'toggleRead');
+        let bookName = document.querySelector(selector + 'titleDiv').textContent;
+           console.log(bookName);
+        let isTitle = (element) => element.title === bookName;
+        let bookIndex = this.myLibrary.findIndex(isTitle);
+
+        if (this.myLibrary[bookIndex].read === 'Read') {
+            this.myLibrary[bookIndex].read = "Not Read"
+            readButton.textContent = "Not Read"
+            readButton.classList.remove('read')
+            readButton.classList.add('notRead')
+            console.log(this.myLibrary);
+
+        } else {
+            this.myLibrary[bookIndex].read = "Read";
+            readButton.textContent = "Read"
+            readButton.classList.remove('notRead')
+            readButton.classList.add('read')
+            console.log(this.myLibrary);
+        }
+
+    }
+
+    removeBook(id) {
+        let newId = id.toString().substring(0, id.toString().length - 12);
+        let selector = '#' + newId;
+        let cardDiv = document.querySelector(selector + 'Card');
+        let bookName = document.querySelector(selector + 'titleDiv').textContent;
+        let isTitle = (element) => element.title === bookName;
+        let bookIndex = this.myLibrary.findIndex(isTitle);
+        cardDiv.remove();
+        this.myLibrary.splice(bookIndex, 1);
+        console.log(this.myLibrary)
+
+
     }
 }
 
-function addBookToLibrary(title, author, pages, read) {
+class Book {
 
-    let book = new Book(title, author, pages, read);
-    myLibrary.push(book);
-
-    let bookDiv = createBookCard(title, author, pages, read);
-    mainContainer.appendChild(bookDiv);
-}
-
-
-function createBookCard(title, author, pages, read) {
-    let cardDiv = document.createElement("div");
-    cardDiv.setAttribute('id', title + 'Card');
-
-
-    let tileDiv = document.createElement("div");
-    let authorDiv = document.createElement("div");
-    let pagesDiv = document.createElement("div");
-
-    let buttonGroup = document.createElement("div");
-    buttonGroup.setAttribute('id', title + 'ButtonGroup')
-    buttonGroup.classList.add('button-group')
-
-    let readDiv = document.createElement("button");
-    let removeButton = document.createElement("button")
-
-    tileDiv.setAttribute('id', title + 'titleDiv');
-
-    removeButton.textContent = "Remove";
-    removeButton.setAttribute('id', title + 'removeButton');
-    removeButton.addEventListener("click", removeBook);
-
-    readDiv.setAttribute('id', title + 'toggleRead');
-    readDiv.addEventListener("click", ChangeReadStatus);
-
-    if(read==='Read'){
-        readDiv.classList.add('read')
-    }else{
-        readDiv.classList.add('notRead')
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
     }
 
-    tileDiv.textContent = title
-    authorDiv.textContent = author;
-    pagesDiv.textContent = pages;
-    readDiv.textContent = read;
-
-    cardDiv.classList.add('book-card');
-
-    cardDiv.appendChild(tileDiv);
-    cardDiv.appendChild(authorDiv);
-    cardDiv.appendChild(pagesDiv);
-
-    buttonGroup.appendChild(readDiv);
-    buttonGroup.appendChild(removeButton);
-
-    cardDiv.appendChild(buttonGroup);
-
-    return cardDiv;
-}
-
-
-function ChangeReadStatus() {
-    let id = this.id.toString().substring(0, this.id.toString().length - 10);
-    console.log(id);
-    let selector = '#' + id;
-    console.log(selector);
-    let readButton = document.querySelector(selector + 'toggleRead');
-    let bookName = document.querySelector(selector + 'titleDiv').textContent;
-    let isTitle = (element) => element.title === bookName;
-    let bookIndex = myLibrary.findIndex(isTitle);
-
-    if (myLibrary[bookIndex].read === 'Read') {
-        myLibrary[bookIndex].read = "Not Read"
-        readButton.textContent = "Not Read"
-        readButton.classList.remove('read')
-        readButton.classList.add('notRead')
-        console.log(myLibrary);
-
-    } else {
-        myLibrary[bookIndex].read = "Read";
-        readButton.textContent = "Read"
-        readButton.classList.remove('notRead')
-        readButton.classList.add('read')
-        console.log(myLibrary);
+    get book() {
+        return (`${this.title} by ${this.author}, ${this.pages}. pages read = ${this.read}`);
     }
 
 }
 
-function removeBook() {
-    let id = this.id.toString().substring(0, this.id.toString().length - 12);
-    let selector = '#' + id;
-    let cardDiv = document.querySelector(selector + 'Card');
-    let bookName = document.querySelector(selector + 'titleDiv').textContent;
-    let isTitle = (element) => element.title === bookName;
-    let bookIndex = myLibrary.findIndex(isTitle);
-    cardDiv.remove();
-    myLibrary.splice(bookIndex, 1);
-    console.log(myLibrary)
 
-
-}
+let L= new Library()
 
 
 addBookButton.addEventListener('click', () => {
@@ -160,13 +174,13 @@ form.addEventListener('submit', function (e) {
     } else {
         read = "Not Read";
     }
-    if (bookExists(titleDiv.value) === true) {
+    if (L.bookExists(titleDiv.value,L.myLibrary) === true) {
         alert(titleDiv.value + " is book already in library!")
         return;
     }
     console.log(read);
-    addBookToLibrary(titleDiv.value, authorDiv.value, pagesDiv.value, read);
-
+    L.addBookToLibrary(titleDiv.value, authorDiv.value, pagesDiv.value, read);
+    console.log(L.library);
     overlay.classList.remove('active');
     form.classList.remove('active');
 
